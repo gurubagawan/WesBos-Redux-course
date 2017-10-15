ff0ad4691e99fee7480a047f47f712373c2fbda6
Guru <guru.bagawan@gmail.com>
2017-10-15 12:36:21 -0400

    'wesbos'

---
+++ learn-redux/client/reducers/posts.js
@@ -0,0 +1,11 @@
+//a reducer takes in two things
+
+// 1. it takes in the action
+// 2. copy of current state
+
+function posts(state=[], action) {
+  console.log(state,action);
+  return state;
+}
+
+export default posts; 
